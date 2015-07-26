98205fd6c0c036f619409671c4b12e1f99fb2180
Panos Kountanis <panosktn@gmail.com>
2015-07-26 16:30:55 +0100

    analysis run rrrrrrrrrrrrwi

--- run_analysis.R
+++ run_analysis.R
@@ -9,10 +9,12 @@ print(paste("using R version ", r.version$version.string))
 if (require("plyr") == FALSE) { install.packages("plyr") }
 if (require("dplyr") == FALSE) { install.packages("dplyr") }
 if (require("reshape2") == FALSE) { install.packages("reshape2") }
+if (require("stringr") == FALSE) { install.packages("stringr") }
 
 library(plyr)
 library(dplyr)
 library(reshape2)
+library(stringr)
 
 # download file if it does not exist and save it in ./data
 
@@ -54,6 +56,8 @@ print(test_set)
 test_labels = file.path(dataset_folder, "test", "y_test.txt")
 print(test_labels)
 
+subject_train_set = file.path(dataset_folder, "train", "subject_train.txt")
+subject_test_set = file.path(dataset_folder, "test", "subject_test.txt")
 
 
 # read feature labels and activity labels
@@ -64,34 +68,37 @@ al_df <- read.table(act_labels_file)
 features_file <- file.path(dataset_folder, "features.txt")
 features_df <- read.table(features_file)
 
-
+subject_train_df <- read.table(subject_train_set)
 # read training_set
+subject_test_df <- read.table(subject_test_set)
 
 tr_x_df <- read.table(training_set)
 tr_y_df <- read.table(training_labels)
-# tr_x_df$activity <- tr_y_df
+# tr_x_df$subject = subject_train_df
+tr_x_df$activity <- tr_y_df
 
 
 te_x_df <- read.table(training_set)
 te_y_df <- read.table(training_labels)
+# te_x_df$subject = subject_test_df
 # te_x_df$activity = NA
-# te_x_df$activity <- as.vector(te_y_df)
+te_x_df$activity <- te_y_df
 
 
-features_transposed <- t(features_df[2])
+# features_transposed <- t(features_df[2])
 
 # Merge training and test sets to create one data set.
-all_data <- rbind(tr_x_df, te_x_df)
-colnames(all_data) <- features_transposed
-d <- seq(1, nrow(all_data), by=1)
-all_data$id <- d
+# all_data <- rbind(tr_x_df, te_x_df)
+# colnames(all_data) <- features_transposed
 
-means <- grep("-mean()", colnames(all_data))
-stds <- grep("-std()", colnames(all_data))
+# means <- grep("-mean\\(\\)$", colnames(all_data))
+# stds <- grep("-std\\(\\)$", colnames(all_data))
 
-all_column_indices = unlist(c(means, stds))
+# all_column_indices = unlist(c(means, stds))
 
-all_data = all_data[all_column_indices]
+# all_data = all_data[all_column_indices]
+# d <- seq(1, nrow(all_data), by=1)
+# all_data$id <- d
 # Extracts only the measurements on the mean and standard deviation for each measurement.
 
 # melt columns
@@ -99,7 +106,9 @@ all_data = all_data[all_column_indices]
 
 # TODO split column variable into 3 columns
 
-# split_cols <- within(melted_data, variable<-data.frame(do.call('rbind', strsplit(as.character(?melted_data$variable),'-',fixed=TRUE))))
+# split_cols <- within(melted_data, variable<-data.frame(do.call('rbind', strsplit(as.character(melted_data$variable),'-',fixed=TRUE))))
+
+# str_split_fixed(melted_data$variable, "-", 2)
 
 # TODO select only columns with means and stddevs
 
