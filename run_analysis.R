c2666535884689de54f3713027f5fd384e53182e
Panos Kountanis <panosktn@gmail.com>
2015-07-26 19:53:09 +0100

    add activity description

--- run_analysis.R
+++ run_analysis.R
@@ -75,34 +75,52 @@ subject_test_df <- read.table(subject_test_set)
 tr_x_df <- read.table(training_set)
 tr_y_df <- read.table(training_labels)
 # tr_x_df$subject = subject_train_df
-tr_x_df$activity <- tr_y_df
-
+# tr_x_df$activity <- as.vector(tr_y_df)
 
 te_x_df <- read.table(training_set)
 te_y_df <- read.table(training_labels)
 # te_x_df$subject = subject_test_df
 # te_x_df$activity = NA
-te_x_df$activity <- te_y_df
+# te_x_df$activity <- as.vector(te_y_df)
+# rownames(te_x_df) <- seq(length(tr_x_df), length(tr_x_df) + length(te_x_df), by=1)
 
+tr_all_y = rbind(tr_y_df, te_y_df)
 
-# features_transposed <- t(features_df[2])
+all_subjects = rbind(subject_train_df, subject_test_df)
+
+features_transposed <- t(features_df[2])
 
 # Merge training and test sets to create one data set.
-# all_data <- rbind(tr_x_df, te_x_df)
-# colnames(all_data) <- features_transposed
+all_data <- rbind(tr_x_df, te_x_df)
 
-# means <- grep("-mean\\(\\)$", colnames(all_data))
-# stds <- grep("-std\\(\\)$", colnames(all_data))
 
-# all_column_indices = unlist(c(means, stds))
+# Extracts only the measurements on the mean and standard deviation for each measurement.
+colnames(all_data) <- features_transposed
 
-# all_data = all_data[all_column_indices]
+means <- grep("-mean\\(\\)$", colnames(all_data))
+stds <- grep("-std\\(\\)$", colnames(all_data))
+
+all_column_indices = unlist(c(means, stds))
+
+all_data = all_data[all_column_indices]
+
+# set activity numbers
+all_data$activity = as.vector(tr_all_y[1])
+all_data$activity = all_data$activity$V1
+# all_data$subject = NA
+# all_data$subject[,0:length(all_subjects)] = all_subjects
 # d <- seq(1, nrow(all_data), by=1)
 # all_data$id <- d
-# Extracts only the measurements on the mean and standard deviation for each measurement.
+
+cutoff <- 0:nrow(all_subjects)
+
+cut_data = all_data[cutoff,]
+
+cut_data$subject = all_subjects
+
 
 # melt columns
-# melted_data <- melt(all_data, id=c("id"))
+# melted_data <- melt(all_data, id=c("id))
 
 # TODO split column variable into 3 columns
 
@@ -113,6 +131,10 @@ te_x_df$activity <- te_y_df
 # TODO select only columns with means and stddevs
 
 
+
 # Uses descriptive activity names to name the activities in the data set
+
+mm = merge(cut_data, al_df, by.x="activity", by.y="V1")
+
 # Appropriatelhy labels the data set with descriptive variable names. 
 # From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
